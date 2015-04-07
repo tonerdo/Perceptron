bee811cd49103ddddfa801ae76860366c50c58b1
Developer Without Borders <toni.edward@outlook.com>
2015-04-07 15:12:25 +0100

    First Perceptron commit

---
+++ Perceptron/Perceptron.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class Perceptron
+    {
+        public double LearningRate { get; set; }
+        public double Threshold { get; set; }
+        public double[][] Inputs { get; set; }
+        public double[] Outputs { get; set; }
+        public double[] Weights { get; set; }
+        private int Length { get; set; }
+
+        public Perceptron()
+        {
+            this.LearningRate = 0.1;
+            this.Threshold = 0.5;
+        }
+
+        public void Train()
+        {
+            this.Length = Weights.Length;
+
+            for (int a = 0; a < 100; a++)
+            {
+                for (int i = 0; i < this.Inputs.Length; i++)
+                {
+                    double[] c = new double[this.Inputs.Length];
+                    double s = 0;
+                    double n = 0;
+                    double e = 0;
+                    double d = 0;
+
+                    for (int j = 0; j < this.Length; j++)
+                    {
+                        c[j] = this.Inputs[i][j] * this.Weights[j];
+                    }
+
+                    s = c.Sum();
+                    n = ActivationFunction(s);
+                    e = this.Outputs[i] - n;
+                    d = this.LearningRate * e;
+
+                    for (int k = 0; k < this.Weights.Length; k++)
+                    {
+                        this.Weights[k] = this.Weights[k] + (this.Inputs[i][k] * d);
+                    }
+
+                    Console.WriteLine("Iteration: " + (a + 1));
+                    Console.WriteLine("Target Output: " + this.Outputs[i]);
+                    Console.WriteLine("Network Output: " + n);
+                    Console.WriteLine("Error: " + e);
+                    Console.Write("Weights: ");
+                    foreach (double weight in this.Weights)
+                    {
+                        Console.Write(weight + " ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            }
+
+        }
+
+        public double ActivationFunction(double output)
+        {
+            if (output > this.Threshold)
+                return 1;
+            else
+                return 0;
+            //return output;
+            //return Math.Round(output);
+        }
+    }
+}
---
+++ Perceptron/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] outputs = new double[] { 1, 1, 1, 1 };
+            double[][] inputs = new double[4][] {
+											new double[] {1, 0},
+											new double[] {0, 1},
+											new double[] {1, 2},
+											new double[] {0, 3}
+										};
+            double[] weights = new double[inputs[0].Length];
+
+            Console.Write("Initial Weights: ");
+            foreach (double weight in weights)
+            {
+                Console.Write(weight + " ");
+            }
+            Console.WriteLine();
+
+            Perceptron perceptron = new Perceptron();
+            perceptron.Inputs = inputs;
+            perceptron.Outputs = outputs;
+            perceptron.Weights = weights;
+            perceptron.Train();
+
+            Console.ReadKey(true);
+        }
+    }
+}
